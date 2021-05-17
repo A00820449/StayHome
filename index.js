4518056f65061fa4cd090d2e25d1d312756d0b03
A00820449 <A00820449@itesm.mx>
2021-05-17 13:50:58 -0500

    added config file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const express = require("express");
+const mysql = require("mysql2");
+const options = require("./config.json");
+
+const port = options.port;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
+});
