7bf46c68de369343e66c289a87fb942bbe2ae229
A00820449 <A00820449@itesm.mx>
2021-05-17 15:39:05 -0500

    added db connection

--- index.js
+++ index.js
@@ -6,8 +6,28 @@ const port = options.port;
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+const mysqloptions = {
+    host    : options.mysql_host,
+    port    : options.mysql_port,
+    user    : options.mysql_user,
+    password: options.mysql_password,
+    database: options.mysql_database,
+
+    waitForConnections: true,
+    connectionLimit:    10,
+    queueLimit:         0
+};
+const dbPool = mysql.createPool(mysqloptions).promise();
+
+app.get("/", async (req, res) => {
+    try {
+        const [rows, fields] = await dbPool.query("SELECT 1 + 1;");
+        res.json(rows);
+    }
+    catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 });
 
 app.listen(port, ()=>{
