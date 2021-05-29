bef19043815a3414931887935113723f1685c381
A00820449 <A00820449@itesm.mx>
2021-05-29 17:50:26 -0500

    merged staff and branch pages

--- routes/branch.js
+++ routes/branch.js
@@ -2,10 +2,28 @@ const express = require("express");
 const router = express.Router();
 const mysqlpool = require("../db_connection_pool");
 
+async function BranchAddStaff(branches, connection) {
+    for (var branch of branches) {
+        branch.Staff = [];
+        [branch.Staff,] = await connection.query(`SELECT * FROM Staff WHERE BranchNo = ?;`, [branch.BranchNo]);
+    }
+    return branches;
+}
+
 router.get("/", async (req, res)=>{
     try {
-        const [rows, fields] = await mysqlpool.query(`SELECT * FROM Branch LIMIT 1000;`);
-        res.render("branch", {branches: rows});
+        const connection = await mysqlpool.getConnection();
+        try {
+            var [rows, fields] = await connection.query(`SELECT * FROM Branch LIMIT 1000;`);
+            rows = await BranchAddStaff(rows, connection);
+            res.render("branch", {branches: rows});
+        }
+        catch (e) {
+            throw e;
+        }
+        finally {
+            connection.release();
+        }
     }
     catch (e) {
         console.log(e);
